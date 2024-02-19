4f7465dd238d7dd7ead5a813a485e6d974c941b6
budhaditya-itobuz <it012303@IT012303s-MacBook-Pro.local>
2024-02-19 15:08:28 +0530

    code indented

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const body =document.querySelector('#body')
-const toggler =document.getElementById('toggle')
-toggler.setAttribute('src',"./images/navbar/moon-solid.svg")
+const body = document.querySelector("#body");
+const toggler = document.getElementById("toggle");
+toggler.setAttribute("src", "./images/navbar/moon-solid.svg");
 
-toggler.addEventListener('click',()=>{
-    body.classList.toggle('dark')
-    if(toggler.getAttribute('src')!="./images/navbar/sun-solid.svg")
-    toggler.setAttribute('src',"./images/navbar/sun-solid.svg")
-    else
-    {
-        toggler.setAttribute('src',"./images/navbar/moon-solid.svg")
-    }
-
-
-
-})
-
+toggler.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (toggler.getAttribute("src") != "./images/navbar/sun-solid.svg")
+    toggler.setAttribute("src", "./images/navbar/sun-solid.svg");
+  else {
+    toggler.setAttribute("src", "./images/navbar/moon-solid.svg");
+  }
+});
