d5a28661698975f15b6a17af6b91d24d7e7b3ef0
budhaditya-itobuz <it012303@IT012303s-MacBook-Pro.local>
2024-02-19 14:36:33 +0530

    Theming added

---
+++ index.js
@@ -0,0 +1,17 @@
+const body =document.querySelector('#body')
+const toggler =document.getElementById('toggle')
+toggler.setAttribute('src',"./images/navbar/moon-solid.svg")
+
+toggler.addEventListener('click',()=>{
+    body.classList.toggle('dark')
+    if(toggler.getAttribute('src')!="./images/navbar/sun-solid.svg")
+    toggler.setAttribute('src',"./images/navbar/sun-solid.svg")
+    else
+    {
+        toggler.setAttribute('src',"./images/navbar/moon-solid.svg")
+    }
+
+
+
+})
+
